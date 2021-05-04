771a55bd9377dae8cede8914f6cbef204a1d5c6f
ItzShiro <65608540+ItzShiro@users.noreply.github.com>
2021-05-04 09:23:04 +0200

    1.0

---
+++ script.js
@@ -0,0 +1,14 @@
+var input = document.getElementById('input');
+var body = document.body;
+
+function doStuff(){
+  console.log(input.value);
+  console.log( "https://nhentai.net/g/" + input.value );
+  location.href = "https://nhentai.net/g/" + input.value 
+}
+
+$(function() {
+        $("input[name='numonly']").on('input', function(e) {
+            $(this).val($(this).val().replace(/[^0-9]/g, ''));
+        });
+    });
